b36b203eeecf7ff28940956ee2cced469f98dfa8
Hans Perez <hanspz1011@gmail.com>
2026-07-23 03:38:17 +0300

    Write logic for computer's random choice
    Created a function with if/else statement for random choices made.

---
+++ script.js
@@ -0,0 +1,15 @@
+let computerChoice = getComputerChoice();
+
+function getComputerChoice () {
+    let randomChoice = Math.random();
+    if (randomChoice <= 0.333) {
+        return "Rock"
+    } else if (randomChoice > 0.333 && randomChoice <= 0.666) {
+        return "Paper" 
+    } else {
+        return "Scissors"
+    }
+}
+
+
+
