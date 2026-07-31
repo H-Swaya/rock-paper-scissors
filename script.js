fd253befbe3c309790924db9829f56250ad64229
Hans Perez <hanspz1011@gmail.com>
2026-07-31 04:23:08 +0300

    Add player choice buttons with event listeners

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
-function getComputerChoice () {
-    let randomChoice = Math.random();
-    if (randomChoice <= 0.333) {
-        return "Rock"
-    } else if (randomChoice > 0.333 && randomChoice <= 0.666) {
-        return "Paper" 
-    } else {
-        return "Scissors"
-    }
+function getComputerChoice() {
+  let randomChoice = Math.random();
+  if (randomChoice <= 0.333) {
+    return "Rock";
+  } else if (randomChoice > 0.333 && randomChoice <= 0.666) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
-let humanScore = 0
+let humanScore = 0;
 
-let computerScore = 0
+let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-    console.log("userChoice = " + humanChoice + " computerChoice = " + computerChoice)
-    // Draw
-    if (humanChoice == computerChoice) {    
-        console.log("It's a draw")
-    } 
-    // Player Won
-    else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore = humanScore + 1
-        console.log("You Won!")
-    }   
-    // Player Lose
-    else {
-        computerScore = computerScore + 1
-        console.log("You Lost!")
-    }
-    console.log("Player Score = " + humanScore + "|| Computer Score = " + computerScore)
+function playRound(humanChoice, getComputerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = getComputerChoice().toLowerCase();
+  console.log(
+    "userChoice = " + humanChoice + " computerChoice = " + computerChoice,
+  );
+  // Draw
+  if (humanChoice == computerChoice) {
+    console.log("It's a draw");
+  }
+  // Player Won
+  else if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    humanScore = humanScore + 1;
+    console.log("You Won!");
+  }
+  // Player Lose
+  else {
+    computerScore = computerScore + 1;
+    console.log("You Lost!");
+  }
+  console.log(
+    "Player Score = " + humanScore + "|| Computer Score = " + computerScore,
+  );
 }
 
-function playGame () {
-    playRound(prompt("Pick one. Rock, paper or scissors?"), getComputerChoice())
-    playRound(prompt("Pick one. Rock, paper or scissors?"), getComputerChoice())
-    playRound(prompt("Pick one. Rock, paper or scissors?"), getComputerChoice())
-    playRound(prompt("Pick one. Rock, paper or scissors?"), getComputerChoice())
-    playRound(prompt("Pick one. Rock, paper or scissors?"), getComputerChoice())
+let rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () =>
+  playRound("rock", getComputerChoice),
+);
 
-    if (humanScore > computerScore) {
-        console.log("CONGRATS, YOU WON THE MATCH")
-    } else if (humanScore < computerScore) {
-        console.log("UNFORTUNATELY, YOU LOST THE MATCH") 
-    } else {
-        console.log("THIS MATCH ENDED UP IN A DRAW!")
-    }
-}
+let paperButton = document.querySelector("#paper");
+paperButton.addEventListener("click", () =>
+  playRound("paper", getComputerChoice),
+);
 
-playGame()
+let scissorButton = document.querySelector("#scissors");
+scissorButton.addEventListener("click", () =>
+  playRound("scissors", getComputerChoice),
+);
